9c457ffc022c7a6be579d02162145f9f29fc3835
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-15 10:59:26 +0300

    finalize on switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,50 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int num = 2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number,between 0 -9, to check");
+        int num = scanner.nextInt();
 
 
-
+        switch (num) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+            {
+                System.out.println("Invalid Input");
+            }
+        }
     }
 }
